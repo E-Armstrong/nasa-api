338dffcbb3c7e2ea5dcf3dcc88c9e8238475f58d
EricA <eggsaladsandwich@Es-Mac.local>
2017-10-25 22:51:10 -0600

    All skeleton structure created (HTML, javascript with data) but minial CSS. Still unreadable.

--- main.js
+++ main.js
@@ -1,45 +1,42 @@
+var dangerousAsteroids = [] //stores all of the raw data for each dangerous asteroid
 
+var javascriptNestedInjQuery = function(data) {
+     
 
-var theRestofTheJavascript = function(data) {
+    // console.log(dangerousAsteroids)
+    // console.log(dangerousAsteroids[0].name)
+    // console.log(dangerousAsteroids[0].estimated_diameter.miles.estimated_diameter_max)
+    // console.log(dangerousAsteroids[0].close_approach_data[0].close_approach_date)
+    // console.log(dangerousAsteroids[0].close_approach_data[0].relative_velocity.miles_per_hour)
+    // console.log(dangerousAsteroids[0].close_approach_data[0].orbiting_body)
+    // console.log(dangerousAsteroids[0].close_approach_data[0].miss_distance.miles)
     
-    var dangerousAsteroids = [] //stores all of the raw data for each dangerous asteroid
     
-    var asteroid = [] //stores just my specific data for each asteroid 
-
-
-    for (let days in data.near_earth_objects) {
-        for (var i = 0; i < data.near_earth_objects[days].length; i++) {
-            if (data.near_earth_objects[days][i].is_potentially_hazardous_asteroid === true) {
-                dangerousAsteroids.push(data.near_earth_objects[days][i])
-            }
-        }
-    }
-    console.log(dangerousAsteroids)
-
-    console.log(dangerousAsteroids[0].estimated_diameter.miles.estimated_diameter_max)
-    console.log(dangerousAsteroids[0].close_approach_data[0].close_approach_date)
-    console.log(dangerousAsteroids[0].close_approach_data[0].relative_velocity.miles_per_hour)
-    console.log(dangerousAsteroids[0].close_approach_data[0].orbiting_body)
-    console.log(dangerousAsteroids[0].close_approach_data[0].miss_distance.miles)
-    
-    var asteroidObject = function(diameter, approach_date, speed, orbiting_body, miss_distance) { //this function will create separate objects for each of my asteriods
-        this.diameter = diameter;
-        this.approach_date = approach_date;
-        this.speed = speed;
-        this.orbiting_body = orbiting_body;
-        this.miss_distance = miss_distance;
-    }
-
-    for (i = 0; i < dangerousAsteroids.length; i++) { 
-        asteroid.push(new asteroidObject(dangerousAsteroids[i].estimated_diameter.miles.estimated_diameter_max, dangerousAsteroids[i].close_approach_data[0].close_approach_date, dangerousAsteroids[i].close_approach_data[0].relative_velocity.miles_per_hour, dangerousAsteroids[i].close_approach_data[0].orbiting_body, dangerousAsteroids[i].close_approach_data[0].miss_distance.miles))
-    }
-    console.log(asteroid)
-
 }
 $(document).ready(function(){ 
     
-     $.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-09-01&end_date=2017-09-07&api_key=oelul3xgp43s2yLjkmIuyJzOYqhKKLCke4XB24nh', function(data) {
-        theRestofTheJavascript(data)   
-     })
-    
+    $.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-09-01&end_date=2017-09-07&api_key=oelul3xgp43s2yLjkmIuyJzOYqhKKLCke4XB24nh', function(data) {
+        for (let days in data.near_earth_objects) {
+            for (var i = 0; i < data.near_earth_objects[days].length; i++) {
+                if (data.near_earth_objects[days][i].is_potentially_hazardous_asteroid === true) {
+                    dangerousAsteroids.push(data.near_earth_objects[days][i])
+                }
+            }
+        }
+        javascriptNestedInjQuery(data)   
+        for (var i =0; i < dangerousAsteroids.length; i++) {
+            
+            $('#main-container').append(`
+            <div class="asteroid">
+            <img src="http://www.daily-sun.com/assets/news_images/2017/06/29/asteroids-humanity-daily-sun.jpg">
+            <div class="name">Name: ${dangerousAsteroids[i].name}</div>
+            <div class="diameter">Diameter: ${dangerousAsteroids[i].estimated_diameter.miles.estimated_diameter_max} miles</div>
+            <div class="approach_date">Approach Date: ${dangerousAsteroids[i].close_approach_data[0].close_approach_date}</div>
+            <div class="speed">Speed: ${dangerousAsteroids[i].close_approach_data[0].relative_velocity.miles_per_hour} Miles Per Hour</div>
+            <div class="orbiting_body">Orbiting Body: ${dangerousAsteroids[i].close_approach_data[0].orbiting_body}</div>
+            <div class="miss_distance">Diameter: ${dangerousAsteroids[i].close_approach_data[0].miss_distance.miles} miles</div>
+            </div>)
+            `)
+        }
+    })
 })
